3d5bc2550dd8f791b460281a6e30bfb3fd1c1165
99jeevi15 <32951415+99jeevi15@users.noreply.github.com>
2017-10-21 10:34:55 +0530

    Add files via upload

---
+++ pppp.py
@@ -0,0 +1,39 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse013
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse013 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    a=20
+    b=5
+    print(a+b)
+if __name__ == '__main__':
+    main()
+
+#python version :3
+a=6+9
+b=5-6
+c=9/5
+d=5%8
+e=3**5
+f=9--6
+g=2<>4
+h=1<8
+i=4>5
+j=8++3
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
